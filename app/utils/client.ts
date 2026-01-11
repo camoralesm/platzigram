d211bdb3413f19e223f8fbfbf7ae8ad643d72c5d
Carlos Morales <camoralesm@unal.edu.co>
2026-01-11 18:20:16 -0500

    feat: initialize project with Next.js,

--- app/utils/client.ts
+++ app/utils/client.ts
@@ -4,4 +4,9 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey); 
